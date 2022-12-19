ce2bd6847b7ce2e4885aa763b38ab2912b06dc7b
majaklysz <eaamkly@students.eaaa.dk>
2022-12-19 17:20:52 +0100

    exam

---
+++ js/js.js
@@ -0,0 +1,68 @@
+// When the user scrolls the page, execute myFunction
+window.onscroll = function () {
+  myFunction();
+};
+
+// Get the navbar
+var navbar = document.getElementById("navbar");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky");
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+
+//side menu
+
+function menuappear() {
+  document.getElementById("appear").style.display = "unset";
+}
+
+function menudisappear() {
+  document.getElementById("appear").style.display = "none";
+}
+
+let menuButton = document.querySelector(".burgerMenu");
+menuButton.addEventListener("click", menuappear);
+
+let menuBackground = document.querySelector(".xbutton");
+menuBackground.addEventListener("click", menudisappear);
+
+//js for accordion (W3school)
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function () {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("active");
+
+    /* Toggle between hiding and showing the active panel */
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
+//slide down
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
---
+++ js/place.js
@@ -0,0 +1,74 @@
+// When the user scrolls the page, execute myFunction
+window.onscroll = function () {
+  myFunction();
+};
+
+// Get the navbar
+var navbar = document.getElementById("navbar");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky");
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+
+//SlideShow
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
+
+//button for adding review
+
+function addingReview() {
+  alert("THANK YOU! Your review was added successfuly! 🤗🎉");
+}
+let revButton = document.querySelector(".addReviewButton");
+revButton.addEventListener("click", addingReview);
+
+//sidemenu
+function menuappear() {
+  document.getElementById("appear").style.display = "unset";
+}
+
+function menudisappear() {
+  document.getElementById("appear").style.display = "none";
+}
+
+let menuButton = document.querySelector(".burgerMenu");
+menuButton.addEventListener("click", menuappear);
+
+let menuBackground = document.querySelector(".xbutton");
+menuBackground.addEventListener("click", menudisappear);
