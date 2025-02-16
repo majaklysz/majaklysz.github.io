25fe5a4b1e7331fad929f183a12a5346ebca9b7e
Maja Kłysz <113166165+majaklysz@users.noreply.github.com>
2025-02-16 08:27:33 +0100

    configuration

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import {defineConfig} from "vite";
 import vue from "@vitejs/plugin-vue";
 
-export default defineConfig({
-  plugins: [vue()],
-  base: "./",
+export default defineConfig(({command}) => {
+  const config = {
+    plugins: [vue()],
+    base: "/",
+  };
+
+  if (command !== "serve") {
+    config.base = "/majaklysz.github.io/";
+  }
+
+  return config;
 });
